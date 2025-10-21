f88c06ac6a25d6fc5de4f080bd4e00e352392a96
Ethan Montes <ethanf@themontes.net>
2025-10-21 12:27:59 -0400

    Added OpenAI's API Key in an env file and defined the transcribe_audio function

--- app.py
+++ app.py
@@ -1,1 +1,20 @@
-print("hello")
+import openai
+from dotenv import load_dotenv
+import os
+
+load_dotenv()
+openai.api_key = os.getenv("OPENAI_API_KEY")
+
+def transcribe_audio(audio_path):
+    """This function will take an audio recording and transcribe it using
+    OpenAI's Whisper Model"""
+    try:
+        with open(audio_path,"rb") as audio_file:
+            transcript = openai.audio.transcriptions.create(
+                model = "whisper-1",
+                file = audio_file
+            )
+            return transcript.text
+    except Exception as e:
+        return f"An error occurred during the transcription: {e}"
+    
